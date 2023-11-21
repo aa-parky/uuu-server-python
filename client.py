c2fa629f5ec2f2e19e5e7de25b2cfcadb4c810d7
aaparky <aa.parky@gmail.com>
2023-11-21 18:52:40 +0000

    working server messages

--- client.py
+++ client.py
@@ -1,26 +1,30 @@
 import asyncio
 import websockets
 import ssl
+import datetime
 
 async def websocket_client():
     uri = "wss://localhost:7450"  # Replace with your server's URI
 
-    # Create an SSL context that does not verify the certificate
     ssl_context = ssl.create_default_context()
     ssl_context.check_hostname = False
     ssl_context.verify_mode = ssl.CERT_NONE
 
     async with websockets.connect(uri, ssl=ssl_context) as websocket:
-        # Receive greeting message from the server
         greeting = await websocket.recv()
-        print(f"Server says: {greeting}")
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{current_time} Server says: {greeting}")
 
-        # Client can reply or send messages to the server
-        message_to_send = input("Enter your message: ")
-        await websocket.send(message_to_send)
-        print("Message sent to the server.")
+        while True:
+            message_to_send = input("> ")
+            await websocket.send(message_to_send)
 
-        # Additional communication logic can be added here
+            response = await websocket.recv()
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{current_time} > {response}")
+
+            if response in ["Login successful.", "Login failed."]:
+                break  # Exit loop after login attempt
 
 # Run the client
 asyncio.run(websocket_client())
--- server_auth.py
+++ server_auth.py
@@ -6,7 +6,6 @@ import mysql.connector
 from mysql.connector import Error
 import datetime
 
-
 # Function to load configuration
 def load_config():
     config = configparser.ConfigParser()
@@ -15,9 +14,8 @@ def load_config():
     db_config = config['Database']
     ssl_config = config['SSL']
     server_config = config['Server']
-    messages = config['Messages']  # Load messages from config.ini
+    messages = config['Messages']
     return registration, db_config, ssl_config, server_config, messages
-
 
 # Function to create a database connection
 def create_db_connection(db_config):
@@ -33,35 +31,60 @@ def create_db_connection(db_config):
         print(f"Error connecting to MariaDB Platform: {e}")
         return None
 
+# Function to check user credentials
+async def check_credentials(websocket, db_config):
+    await websocket.send("Enter username:")
+    username = await websocket.recv()
+
+    await websocket.send("Enter password:")
+    password = await websocket.recv()
+
+    # Connect to the database and verify credentials
+    connection = create_db_connection(db_config)
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT password FROM users WHERE username = %s", (username,))
+            user_password = cursor.fetchone()
+            cursor.close()
+            connection.close()
+            if user_password and user_password[0] == password:
+                return True
+            else:
+                return False
+        except Error as e:
+            print(f"Database error: {e}")
+            return False
+    else:
+        return False
 
 # Websocket server handler
 async def server_handler(websocket, path):
-    # Load config
     registration, db_config, ssl_config, server_config, messages = load_config()
-
-    # Choose the right greeting message
-    greeting_message = messages['greeting_with_registration'] if registration else messages[
-        'greeting_without_registration']
-
-    # Send the greeting message to the client
+    greeting_message = messages['greeting_with_registration'] if registration else messages['greeting_without_registration']
     await websocket.send(greeting_message)
 
-    # Here you can add more interaction logic with the client if needed
-
+    try:
+        while True:
+            message = await websocket.recv()
+            if message.lower() == "login":
+                login_success = await check_credentials(websocket, db_config)
+                if login_success:
+                    await websocket.send("Login successful.")
+                else:
+                    await websocket.send("Login failed.")
+            else:
+                await websocket.send("Unknown command. Try 'login'.")
+    except websockets.exceptions.ConnectionClosed:
+        print("Connection closed.")
 
 # Start the websocket server
 async def start_server():
-    # Load config
     _, _, ssl_config, server_config, _ = load_config()
-
-    # Setup SSL context
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     ssl_context.load_cert_chain(ssl_config['certfile'], ssl_config['keyfile'])
-
-    # Start the server
     async with websockets.serve(server_handler, server_config['host'], int(server_config['port']), ssl=ssl_context):
-        await asyncio.Future()  # Run the server until it's manually stopped
-
+        await asyncio.Future()
 
 # Run the server
 asyncio.run(start_server())
