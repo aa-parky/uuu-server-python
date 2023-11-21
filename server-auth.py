7c0af75e653abc0f42f0a23b4aba1927a81e9b90
aaparky <aa.parky@gmail.com>
2023-11-21 12:49:25 +0000

    removed from server-auth added to config.ini

--- server-auth.py
+++ server-auth.py
@@ -4,13 +4,18 @@ import ssl
 import configparser
 import mysql.connector
 from mysql.connector import Error
-import datetime  # Import the datetime module for logging
+import datetime
 
 # Function to load configuration
 def load_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
-    return config.getboolean('Settings', 'Registration'), config['Database']
+    registration = config.getboolean('Settings', 'Registration')
+    db_config = config['Database']
+    ssl_config = config['SSL']
+    server_config = config['Server']
+    messages = config['Messages']  # Load messages from config.ini
+    return registration, db_config, ssl_config, server_config, messages
 
 # Function to create a database connection
 def create_db_connection(db_config):
@@ -44,7 +49,7 @@ async def check_user_credentials(username, password, db_config):
             record = cursor.fetchone()
             cursor.close()
             connection.close()
-            if record and password == record[0]:  # Simplified for demonstration, use hashed password comparison
+            if record and password == record[0]:
                 return True, "Login successful"
             else:
                 return False, "Invalid username or password"
@@ -54,17 +59,11 @@ async def check_user_credentials(username, password, db_config):
         return False, "Database connection error"
 
 # Echo function with registration logic
-async def handle_client(websocket, path, registration_enabled, db_config):
-    greeting_message = "Sparkfuse Signals here! Login with [login]"
-    if registration_enabled:
-        greeting_message += " or Register with [register]"
-    else:
-        greeting_message += ". Registration currently closed"
-
+async def handle_client(websocket, path, registration_enabled, db_config, messages):
+    greeting_message = messages['greeting_with_registration'] if registration_enabled else messages['greeting_without_registration']
     await websocket.send(greeting_message)
 
     async for message in websocket:
-        # Parse the message and perform actions based on the content.
         print(f"Received message: {message}")
         parts = message.split()
         attempted = "Unknown"
@@ -81,12 +80,13 @@ async def handle_client(websocket, path, registration_enabled, db_config):
 
 async def main():
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    ssl_context.load_cert_chain(certfile="C:/MAMP/bin/certs/uuu-websocket-test.cert",
-                                keyfile="C:/MAMP/bin/certs/uuu-websocket-test.key")
+    registration_enabled, db_config, ssl_config, server_config, messages = load_config()
+    ssl_context.load_cert_chain(certfile=ssl_config['certfile'], keyfile=ssl_config['keyfile'])
+    server_host = server_config['host']
+    server_port = int(server_config['port'])
 
-    registration_enabled, db_config = load_config()
-
-    async with websockets.serve(lambda ws, path: handle_client(ws, path, registration_enabled, db_config), "localhost", 7450, ssl=ssl_context):
+    async with websockets.serve(lambda ws, path: handle_client(ws, path, registration_enabled, db_config, messages),
+                                server_host, server_port, ssl=ssl_context):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
