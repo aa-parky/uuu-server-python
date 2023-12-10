2f7cb2d5d9ffff45eefccbfe5880e5389add3d20
aaparky <aa.parky@gmail.com>
2023-12-10 11:46:15 +0000

    created pip requirements

--- database.py
+++ database.py
@@ -8,7 +8,8 @@ def create_db_connection(db_config):
             host=db_config['host'],
             user=db_config['user'],
             password=db_config['password'],
-            database=db_config['database']
+            database=db_config['database'],
+            port=db_config.get('port', 3306)
         )
         return connection
     except Error as e:
